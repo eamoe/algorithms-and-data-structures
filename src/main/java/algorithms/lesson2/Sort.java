6a9ec8c0a64fcbced3db1bb8c197438ab3e45698
Eugene Menski <eugene.menski@gmail.com>
2023-01-17 18:21:40 +0400

    Added insertSort method

--- src/main/java/algorithms/lesson2/Sort.java
+++ src/main/java/algorithms/lesson2/Sort.java
@@ -4,13 +4,14 @@ public class Sort {
 
     public static void main(String[] args) {
         int[] array = new int[]{4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5};
-        directSort(array);
+        insertSort(array);
 
         for (int item : array) {
             System.out.print(item + " ");
         }
     }
 
+    // Пузырьковая сортировка
     public static void bubbleSort(int[] array) {
         boolean keepDoing;
         do {
@@ -27,6 +28,7 @@ public class Sort {
         } while (!keepDoing);
     }
 
+    // Сортировка выбором
     public static void directSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             int minPosition = i;
@@ -39,6 +41,19 @@ public class Sort {
                 int temp = array[i];
                 array[i] = array[minPosition];
                 array[minPosition] = temp;
+            }
+        }
+    }
+
+    // Сортировка вставками
+    public static void insertSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
         }
     }
