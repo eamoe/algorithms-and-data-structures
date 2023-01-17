2a75480d3f6bac005038dfc664485ae74a0f40fb
Eugene Menski <eugene.menski@gmail.com>
2023-01-17 22:47:24 +0400

    Added quick sort

--- src/main/java/algorithms/lesson2/Sort.java
+++ src/main/java/algorithms/lesson2/Sort.java
@@ -4,7 +4,7 @@ public class Sort {
 
     public static void main(String[] args) {
         int[] array = new int[]{4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5};
-        insertSort(array);
+        quickSort(array, 0, 10);
 
         for (int item : array) {
             System.out.print(item + " ");
@@ -55,6 +55,36 @@ public class Sort {
                     array[j] = temp;
                 }
             }
+        }
+    }
+
+    public static void quickSort(int[] array, int startPosition, int endPosition) {
+        int leftPosition = startPosition;
+        int rightPosition = endPosition;
+        int pivot = array[(startPosition + endPosition) / 2];
+        do {
+            while (array[leftPosition] < pivot) {
+                leftPosition++;
+            }
+            while (array[rightPosition] > pivot) {
+                rightPosition--;
+            }
+            if (leftPosition <= rightPosition) {
+                if (leftPosition < rightPosition) {
+                    int temp = array[leftPosition];
+                    array[leftPosition] = array[rightPosition];
+                    array[rightPosition] = temp;
+                }
+                leftPosition++;
+                rightPosition--;
+            }
+        } while (leftPosition <= rightPosition);
+
+        if (leftPosition < endPosition) {
+            quickSort(array, leftPosition, endPosition);
+        }
+        if (startPosition < rightPosition) {
+            quickSort(array, startPosition, rightPosition);
         }
     }
 
