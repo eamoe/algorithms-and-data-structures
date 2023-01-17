4965b385fc6012591216970a8042c8f31fe8bbcf
Eugene Menski <eugene.menski@gmail.com>
2023-01-17 11:48:03 +0400

    Added directSort method

--- src/main/java/algorithms/lesson2/Sort.java
+++ src/main/java/algorithms/lesson2/Sort.java
@@ -4,7 +4,7 @@ public class Sort {
 
     public static void main(String[] args) {
         int[] array = new int[]{4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5};
-        bubbleSort(array);
+        directSort(array);
 
         for (int item : array) {
             System.out.print(item + " ");
@@ -25,6 +25,22 @@ public class Sort {
             }
 
         } while (!keepDoing);
+    }
+
+    public static void directSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minPosition = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minPosition]) {
+                    minPosition = j;
+                }
+            }
+            if (i != minPosition) {
+                int temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;
+            }
+        }
     }
 
 }
